bfeed828797ba91f83cfe98e72f6a81de7ee41bf
DHAN <MYSTIC@Deborah-Han.local>
2021-04-28 11:18:45 -0700

    Updated doc.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello world!");
+const ham = document.querySelector('.hamburger');
+const modal = document.querySelector('#mainModal');
+const btn = document.querySelector('.navigation');
+const close = document.querySelector('.hamburger-close');
+
+/*
+function myFunction(x) {
+  x.classList.toggle('change');
+}*/
+
+ham.addEventListener("click", function(evt) {
+  modal.style.display="block"
+  document.body.style.overflowY="hidden"
+  document.body.style.height="100vh"
+});
+
+close.addEventListener("click", function(evt) {
+  modal.style.display="none"
+});
+
